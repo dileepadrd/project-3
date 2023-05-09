7b2e7bef8ceecce544347f0707837df0863852da
dileepa <dileeparanjan@gmail.com>
2023-05-08 21:53:39 -0400

    Adding the contract

---
+++ src/solidity/contract.sol
@@ -0,0 +1,154 @@
+pragma solidity ^0.8.0;
+
+contract SupplyChain {
+    // Define the structure of the product
+    struct Product {
+        string name;
+        uint256 quantity;
+        uint256 price;
+        uint256 deliveryDate;
+    }
+
+    // Define the structure of the contract
+    struct Contract {
+        Product product;
+        address payable supplier;
+        address payable buyer;
+        address payable logisticsProvider;
+        uint256 payment;
+        bool paymentDone;
+        bool deliveryDone;
+    }
+
+    // Define the mapping of contracts
+    mapping(bytes32 => Contract) public contracts;
+
+    // Array to store the contract ids
+    bytes32[] contractIds;
+
+    // Number of contracts
+    uint256 public contractCount = 0;
+
+    // Define the event for contract creation
+    event ContractCreated(bytes32 id);
+
+    // Define the event for payment
+    event PaymentReceived(bytes32 id);
+
+    // Define the event for delivery
+    event DeliveryReceived(bytes32 id);
+
+    // Create a new contract
+    function createContract(
+        string memory _name,
+        uint256 _quantity,
+        uint256 _price,
+        uint256 _deliveryDate,
+        address payable _buyer,
+        address payable _logisticsProvider
+    ) public {
+        // Increase the contract count
+        contractCount++;
+
+        // Generate a unique ID for the contract
+        bytes32 id = keccak256(
+            abi.encodePacked(msg.sender, _buyer, block.timestamp)
+        );
+
+        // Define the product details
+        Product memory product = Product(
+            _name,
+            _quantity,
+            _price,
+            _deliveryDate
+        );
+
+        // Define the contract details
+        Contract memory contractDetails = Contract(
+            product,
+            payable(msg.sender),
+            _buyer,
+            _logisticsProvider,
+            _price * _quantity,
+            false,
+            false
+        );
+
+        // Add the contract to the mapping
+        contracts[id] = contractDetails;
+
+        // Add the contract id to the array
+        contractIds.push(id);
+
+        // Emit the contract creation event
+        emit ContractCreated(id);
+    }
+
+    // Send payment to the smart contract's address
+    function sendPayment(bytes32 _id) public payable {
+        // Get the contract details
+        Contract storage contractDetails = contracts[_id];
+
+        // Verify that the payment matches the contract amount
+        require(
+            msg.value == contractDetails.payment,
+            "Payment amount is incorrect"
+        );
+
+        // Mark the payment as done
+        contractDetails.paymentDone = true;
+
+        // Emit the payment event
+        emit PaymentReceived(_id);
+    }
+
+    // Record product delivery on the blockchain
+    function recordDelivery(bytes32 _id) public {
+        // Get the contract details
+        Contract storage contractDetails = contracts[_id];
+
+        // Verify that the sender is the logistics provider
+        require(
+            msg.sender == contractDetails.logisticsProvider,
+            "You are not authorized to record delivery"
+        );
+
+        // Mark the delivery as done
+        contractDetails.deliveryDone = true;
+
+        // Emit the delivery event
+        emit DeliveryReceived(_id);
+    }
+
+    // Confirm the transaction on the blockchain
+    function confirmTransaction(bytes32 _id) public {
+        // Get the contract details
+        Contract storage contractDetails = contracts[_id];
+
+        // Verify that the sender is the buyer
+        require(
+            msg.sender == contractDetails.buyer,
+            "You are not authorized to confirm the transaction"
+        );
+
+        // Verify that the delivery is done and the payment is done
+        require(
+            contractDetails.deliveryDone == true &&
+                contractDetails.paymentDone == true,
+            "Delivery and payment are not complete"
+        );
+
+        // Release the payment to the supplier
+        contractDetails.supplier.transfer(contractDetails.payment);
+    }
+
+    function viewContractIds() public view returns (bytes32[] memory) {
+        return contractIds;
+    }
+
+    function getContract(
+        bytes32 contractId
+    ) public view returns (Contract memory) {
+        return contracts[contractId];
+    }
+}
--- src/tracking-service/api.py
+++ src/tracking-service/api.py
@@ -1,12 +1,72 @@
 from flask import Flask, request, jsonify
+
+import os
+import json
+from web3 import Web3
+from pathlib import Path
+from dotenv import load_dotenv
+
 app = Flask(__name__)
+contract_address = ""
+
 
 @app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
 def add_message(uuid):
     content = request.json
 
-    print(content)
-    return jsonify({"uuid":uuid})
+    accounts = w3.eth.accounts
+    print(accounts)
 
+    from_address = "0xD89C9c336B67E8bAC38e057b18f8b4be155a0a3D"
+
+    name = "contract_1"
+    quantity = 2
+    price = 100
+    deliveryDate = 20202323
+    buyer = "0xD89C9c336B67E8bAC38e057b18f8b4be155a0a3D"
+    logisticsProvider = "0xD89C9c336B67E8bAC38e057b18f8b4be155a0a3D"
+
+    tx_hash = contract.functions.createContract(name, quantity, price, deliveryDate, buyer, logisticsProvider).transact({
+        'from': from_address, 'gas': 1000000})
+
+    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    print(dict(receipt))
+
+    print(content)
+    return jsonify({"uuid": uuid})
+
+
+load_dotenv()
+
+# Define and connect a new Web3 provider
+w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+
+
+def load_contract():
+    # load the contract to be used in the API
+
+    # Load the contract ABI
+    with open(Path('./res/abi.json')) as f:
+        artwork_abi = json.load(f)
+
+    # Set the contract address (this is the address of the deployed contract)
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+    print(f"contract address = {contract_address}\n")
+
+    # Get the contract
+    contract = w3.eth.contract(
+        address=contract_address,
+        abi=artwork_abi
+    )
+
+    return contract
+
+
+# Load the contract
+contract = load_contract()
+
+print("AAAAAAAAAAAAAAAAAAAAAAAAAAA\n")
+
+# Start the application
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
