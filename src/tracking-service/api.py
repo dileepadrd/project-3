13256344604bcddad84417bfbdf2148b209d2d00
dileepa <dileeparanjan@gmail.com>
2023-05-09 20:39:02 -0400

    Adding get contract count and get contract by index function

--- src/tracking-service/api.py
+++ src/tracking-service/api.py
@@ -17,14 +17,14 @@ def create_contract(uuid):
     accounts = w3.eth.accounts
     print(accounts)
 
-    from_address = "0xD89C9c336B67E8bAC38e057b18f8b4be155a0a3D"
+    from_address = "0x33d95947E7D746142966683FA6F63Fa7B4AE9e6d"
 
     name = "contract_1"
     quantity = 2
     price = 100
     deliveryDate = 20202323
-    buyer = "0xD89C9c336B67E8bAC38e057b18f8b4be155a0a3D"
-    logisticsProvider = "0xD89C9c336B67E8bAC38e057b18f8b4be155a0a3D"
+    buyer = "0x33d95947E7D746142966683FA6F63Fa7B4AE9e6d"
+    logisticsProvider = "0x57E11ac1c9bCfe0501055ba550D00B363862AD2b"
 
     tx_hash = contract.functions.createContract(name, quantity, price, deliveryDate, buyer, logisticsProvider).transact(
         {'from': from_address, 'gas': 1000000})
@@ -41,8 +41,28 @@ def contract_ids():
 
     ids = contract.functions.viewContractIds().call()
     print(ids)
+    strIds = []
 
-    return jsonify(ids)
+    for id in ids:
+        decodedId = w3.util.toAscii(id)
+        print(f"type = {type(id)} id = {id} decoded = {decodedId}\n")
+        strIds.append(decodedId)
+
+    return jsonify(strIds)
+
+
+@app.route('/api/v1/contract_count', methods=['GET', 'POST'])
+def get_contract_count():
+    count = contract.functions.getContractCount().call()
+    return jsonify({"count": count})
+
+
+@app.route('/api/v1/contract/<contractIndex>', methods=['GET', 'POST'])
+def view_contract_by_index(contractIndex):
+    contractIndex = int(contractIndex)
+    contractData = contract.functions.getContractByIndex(contractIndex).call()
+    print(f"contract data = {str(contractData)}")
+    return jsonify({"contract": contractData})
 
 
 load_dotenv()
